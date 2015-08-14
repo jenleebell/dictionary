02d226f9b22ce46d4d1b38ccdc5eb0f29a08682e
Jennifer Bell <student@epicodus.com>
2015-08-14 11:04:29 -0700

    Add index file to display word entries from app.rb

--- app.rb
+++ app.rb
@@ -3,3 +3,14 @@ require('sinatra/reloader')
 require('./lib/word')
 require('./lib/definition')
 also_reload('lib/**/*.rb')
+
+get('/') do
+  @word_entries = Word.all()
+  erb(:index)
+end
+
+# post ('/new_word') do
+#   word_entry = params.fetch('word_entry')
+#   new_word_entry = Word.new(word_entry)
+#   new_word.save()
+#   @details =
